a4fdf4f8cc8b6130a2d1af787baa4ceadbe1f3eb
Beriff <58127431+Beriff@users.noreply.github.com>
2020-04-04 10:16:58 +0700

    added command to take items from chests

--- game.py
+++ game.py
@@ -35,7 +35,7 @@ w_chest.objectID = -1
 w_chest.inner_storage = []
 w_chest.capacity = 100
 
-#creating player and his "trail"
+#creating player
 player = ge.Player()
 player.standingOn = rock
 
@@ -72,7 +72,7 @@ currentGrid = grid
 #screen updates when True is in this list
 changes = []
 
-#For headstart and pickaxe creating
+#For headstart and pickaxe creating, and fuck loops
 player.inv.append(rock)
 player.inv.append(rock)
 player.inv.append(rock)
@@ -136,7 +136,7 @@ EastNear = True
 WestNear = True
 
 def userInputDefine(plrInput):
-    if plrInput.startswith("eq"):
+    if plrInput.startswith("eq"): #equip item
         splitted = plrInput.split()
         for i in range(0, len(player.inv)):
             if player.inv[i].name == splitted[1]:
@@ -144,15 +144,15 @@ def userInputDefine(plrInput):
                 print(player.inv[i].name + " equipped")
                 player.inv.pop(i)
                 break
-    if plrInput.startswith("deq"):
+    if plrInput.startswith("deq"): #de-equip item
         splitted = plrInput.split()
         if splitted[1] == player.eq.name and player.eq != empty:
             player.inv.append(player.eq)
             player.eq = empty
             print(splitted[1] + " de-equipped")
-    if plrInput.startswith("dep-e"):
+    if plrInput.startswith("dep-e"): #deposit items to the chest
         try:
-            if EastNear == True:
+            if EastNear:
                 splitted = plrInput.split()
                 for i in range(0, len(player.inv)):
                     if splitted[1] == player.inv[i].name:
@@ -163,11 +163,11 @@ def userInputDefine(plrInput):
             else:
                 print("Can't spot east chest")
         except AttributeError:
-            print("Tried to access not existing chest")
+            print("Tried to access storage of not chest object")
         
     if plrInput.startswith("dep-w"):
         try:
-            if WestNear == True:
+            if WestNear:
                 splitted = plrInput.split()
                 for i in range(0, len(player.inv)):
                     if splitted[1] == player.inv[i].name:
@@ -178,11 +178,11 @@ def userInputDefine(plrInput):
             else:
                 print("Can't spot west chest")
         except AttributeError:
-            print("Tried to access not existing chest")
+            print("Tried to access storage of not chest object")
 
     if plrInput.startswith("dep-n"):
         try:
-            if NorthNear == True:
+            if NorthNear:
                 splitted = plrInput.split()
                 for i in range(0, len(player.inv)):
                     if splitted[1] == player.inv[i].name:
@@ -193,11 +193,11 @@ def userInputDefine(plrInput):
             else:
                 print("Can't spot north chest")
         except AttributeError:
-            print("Tried to access not existing chest")
+            print("Tried to access storage of not chest object")
 
     if plrInput.startswith("dep-s"):
         try:
-            if SouthNear == True:
+            if SouthNear:
                 splitted = plrInput.split()
                 for i in range(0, len(player.inv)):
                     if splitted[1] == player.inv[i].name:
@@ -208,7 +208,68 @@ def userInputDefine(plrInput):
             else:
                 print("Can't spot south chest")
         except AttributeError:
-            print("Tried to access not existing chest")
+            print("Tried to access storage of not chest object")
+
+    if plrInput.startswith("t-e"):
+        try:
+            if EastNear:
+                splitted = plrInput.split()
+                for i in range(0, len(currentGrid.grid[playerPosition + 1].inner_storage)):
+                    if splitted[1] == currentGrid.grid[playerPosition + 1].inner_storage[i].name:
+                        player.inv.append(currentGrid.grid[playerPosition + 1].inner_storage[i])
+                        print("added {0} to your inventory".format(currentGrid.grid[playerPosition + 1].inner_storage[i].name))
+                        currentGrid.grid[playerPosition + 1].inner_storage.pop(i)
+                        break
+            else:
+                print("can't spot east chest")
+        except AttributeError:
+            print("Tried to access storage of not chest object")
+
+    if plrInput.startswith("t-w"):
+        try:
+            if WestNear:
+                splitted = plrInput.split()
+                for i in range(0, len(currentGrid.grid[playerPosition - 1].inner_storage)):
+                    if splitted[1] == currentGrid.grid[playerPosition - 1].inner_storage[i].name:
+                        player.inv.append(currentGrid.grid[playerPosition - 1].inner_storage[i])
+                        print("added {0} to your inventory".format(currentGrid.grid[playerPosition - 1].inner_storage[i].name))
+                        currentGrid.grid[playerPosition - 1].inner_storage.pop(i)
+                        break
+            else:
+                print("can't spot north chest")
+        except AttributeError:
+            print("Tried to access storage of not chest object")
+
+    if plrInput.startswith("t-n"):
+        try:
+            if NorthNear:
+                splitted = plrInput.split()
+                for i in range(0, len(currentGrid.grid[playerPosition - (currentGrid.gridWidth + 1)].inner_storage)):
+                    if splitted[1] == currentGrid.grid[playerPosition - (currentGrid.gridWidth + 1)].inner_storage[i].name:
+                        player.inv.append(currentGrid.grid[playerPosition - (currentGrid.gridWidth + 1)].inner_storage[i])
+                        print("added {0} to your inventory".format(currentGrid.grid[playerPosition - (currentGrid.gridWidth + 1)].inner_storage[i].name))
+                        currentGrid.grid[playerPosition - (currentGrid.gridWidth + 1)].inner_storage.pop(i)
+                        break
+            else:
+                print("can't spot north chest")
+        except AttributeError:
+            print("Tried to access storage of not chest object")
+
+    if plrInput.startswith("t-s"):
+        try:
+            if SouthNear:
+                splitted = plrInput.split()
+                for i in range(0, len(currentGrid.grid[playerPosition + (currentGrid.gridWidth + 1)].inner_storage)):
+                    if splitted[1] == currentGrid.grid[playerPosition + (currentGrid.gridWidth + 1)].inner_storage[i].name:
+                        player.inv.append(currentGrid.grid[playerPosition + (currentGrid.gridWidth + 1)].inner_storage[i])
+                        print("added {0} to your inventory".format(currentGrid.grid[playerPosition + (currentGrid.gridWidth + 1)].inner_storage[i].name))
+                        currentGrid.grid[playerPosition + (currentGrid.gridWidth + 1)].inner_storage.pop(i)
+                        break
+            else:
+                print("can't spot south chest")
+        except AttributeError:
+            print("Tried to access storage of not chest object")
+    
 
 
             
@@ -385,24 +446,24 @@ def key_listen():
     #BLOCK BREAKING AND PLACING USING ARROWS
     #almost everything here is ctrl+c ctrl+v
     try:
-        global log
-        if kb.is_pressed("right arrow") and currentGrid.grid[ playerPosition + 1 ].collision and player.eq.toolID == 1 and ( (playerPosition + 2) % 100 != 0):
-            if currentGrid.grid[ playerPosition + 1 ].level < player.eq.level:
-                if len(player.inv) < player.invCapacity:
-                    player.inv.append(currentGrid.grid[playerPosition + 1])
-                    currentGrid.grid[playerPosition + 1] = currentGrid.grid[playerPosition + 1].bottomTile
+        global log                                #if obj next to player have collision                    #eq tool type = obj breakability type                           #hit will not go out of grid
+        if kb.is_pressed("right arrow") and currentGrid.grid[ playerPosition + 1 ].collision and player.eq.toolID == currentGrid.grid[ playerPosition + 1 ].toolID and ( (playerPosition + 2) % 100 != 0):
+            if currentGrid.grid[ playerPosition + 1 ].level < player.eq.level:      #breakable object's level lower than pickaxe's
+                if len(player.inv) < player.invCapacity:                            #if inventory isn't full
+                    player.inv.append(currentGrid.grid[playerPosition + 1])         #firstly adding item to inventory
+                    currentGrid.grid[playerPosition + 1] = currentGrid.grid[playerPosition + 1].bottomTile  #then replacing obj on grid
                 else:
                     log = "INVENTORY FULL"
-                currentGrid.draw(0)
-                print("HP {0}/{1}\tLOG: {2}\tEQUIPPED:{3}\tBIOME:{4}".format(player.currentHp, player.maxHp, log, player.eq.name, currentLayerBiome.name))
+                currentGrid.draw(0) #updating grid
+                print("HP {0}/{1}\tLOG: {2}\tEQUIPPED:{3}\tBIOME:{4}".format(player.currentHp, player.maxHp, log, player.eq.name, currentLayerBiome.name)) #and additionally drawing bottom bar
 
-                player.eq.durability -= 1
+                player.eq.durability -= 1       #lowering pickaxe's (or axe's) durability
                 if player.eq.durability <= 0:
                     player.eq = empty
                     log = "PICKAXE BROKE"
-        elif kb.is_pressed("right arrow") and not (currentGrid.grid[ playerPosition + 1 ].collision) and player.eq.toolID == 0 and currentGrid.grid[playerPosition + 1] != player.eq:
+        elif kb.is_pressed("right arrow") and not (currentGrid.grid[ playerPosition + 1 ].collision) and player.eq.toolID == 0 and currentGrid.grid[playerPosition + 1] != player.eq: #obj placing mode
             if player.eq.isPlaceable:
-                autoreplace = player.eq
+                autoreplace = player.eq                 #adding same type obj to equipped, so you can use "rock x4" four times without equipping them every time
                 currentGrid.grid[playerPosition + 1] = player.eq
                 player.eq = empty
                 currentGrid.draw()
